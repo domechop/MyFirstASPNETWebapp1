d790527a3bc4f0064d4c2ed569ef9bcdcd2f6c08
domechop <akgarry@gmail.com>
2022-01-20 23:06:01 -0700

    hello

--- MyFirstASPNETWebapp1/Models/GradeCalc.cs
+++ MyFirstASPNETWebapp1/Models/GradeCalc.cs
@@ -9,16 +9,20 @@ namespace MyFirstASPNETWebapp1.Models
     public class GradeCalc
     {
         [Required]
-        [Range(0, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Range(0, 100)]
 
         public int Assignments { get; set; }
-        
+        [Required]
+        [Range(0, 100)]
         public int Group { get; set; }
-       
+        [Required]
+        [Range(0, 100)]
         public int Quizzes { get; set; }
-        
+        [Required]
+        [Range(0, 100)]
         public int Exam { get; set; }
-        
+        [Required]
+        [Range(0, 100)]
         public int Intex { get; set; }
 
     }
